81c496d4a73d510f98b4110c97f0eacb1ea7d695
yerassyl <aitkazy.smartex@gmail.com>
2019-06-17 16:38:50 +0600

    acc creator

--- src/RequestScreen.js
+++ src/RequestScreen.js
@@ -8,6 +8,7 @@ const RequestScreen = props => {
     <div className="p-5 bg-primary text-light rounded w-25">
       <p className="h2">{data.result}</p>
       <p className="h5">{data.message}</p>
+      <br />
       <button onClick={goTo} type="button" className="btn btn-dark">
         Вернуться на главную
       </button>
